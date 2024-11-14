8791430d7bcff99347845a146db68f449a609686
Danilo Manchon <danilomanchon@gmail.com>
2024-11-14 19:07:04 -0300

    initial commit

---
+++ src/test/java/com/techchallenge/produtos/ProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techchallenge.produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
